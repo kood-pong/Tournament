8f809bae70654f47383d1e117bd81b714013d14e
Olya <balagush.olga01@gmail.com>
2024-02-26 19:14:09 +0200

    KOOD-17 fixes

--- front-end/src/components/assets/Sun.tsx
+++ front-end/src/components/assets/Sun.tsx
@@ -2,14 +2,14 @@ const Sun = () => {
     return (
         <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12.4997" cy="12.4999" r="4.16667" fill="var(--sun-color)" />
-            <path d="M12.5 5.20833V3.125" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
-            <path d="M12.5 21.8751V19.7917" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
-            <path d="M17.6558 7.34374L19.1289 5.87061" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
-            <path d="M5.87061 19.1296L7.34375 17.6565" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
-            <path d="M19.7917 12.5L21.875 12.5" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
-            <path d="M3.12467 12.5L5.20801 12.5" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
-            <path d="M17.6558 17.6563L19.1289 19.1294" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
-            <path d="M5.87061 5.87037L7.34375 7.34351" stroke="var(--sun-color)" stroke-width="2" stroke-linecap="round" />
+            <path d="M12.5 5.20833V3.125" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
+            <path d="M12.5 21.8751V19.7917" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
+            <path d="M17.6558 7.34374L19.1289 5.87061" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
+            <path d="M5.87061 19.1296L7.34375 17.6565" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
+            <path d="M19.7917 12.5L21.875 12.5" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
+            <path d="M3.12467 12.5L5.20801 12.5" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
+            <path d="M17.6558 17.6563L19.1289 19.1294" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
+            <path d="M5.87061 5.87037L7.34375 7.34351" stroke="var(--sun-color)" strokeWidth="2" strokeLinecap="round" />
         </svg>
     );
 }
