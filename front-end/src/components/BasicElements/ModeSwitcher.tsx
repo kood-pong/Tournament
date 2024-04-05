db33de9055a476227a2d00e10f50cf77de3a6b21
Olya <balagush.olga01@gmail.com>
2024-04-05 13:50:54 +0300

    theme fixes

--- front-end/src/components/BasicElements/ModeSwitcher.tsx
+++ front-end/src/components/BasicElements/ModeSwitcher.tsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Moon from "../assets/Moon";
 import Sun from "../assets/Sun";
 
 const ModeSwitcher = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
+    useEffect(() => {
+        initTheme();
+    }, []);
+    
+    // Function to initialize the theme
+    const initTheme = () => {
+        // Check if a theme preference is saved in localStorage
+        const savedTheme = localStorage.getItem('theme');
+        
+        if (savedTheme) {
+            // If there is a saved theme, apply it
+            document.documentElement.setAttribute('data-theme', savedTheme);
+            setIsDarkMode(savedTheme === 'dark');
+        } else {
+            // If there is no saved theme, you can default to 'light' or 'dark'
+            // Or, optionally use a system preference check here
+            const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+            const defaultTheme = prefersDarkMode ? 'dark' : 'light';
+            document.documentElement.setAttribute('data-theme', defaultTheme);
+            setIsDarkMode(defaultTheme === 'dark');
+        }
+    }
+    
     const toggleMode = () => {
-        setIsDarkMode(prevMode => !prevMode);
-        document.documentElement.setAttribute('data-theme', isDarkMode ? 'light' : 'dark');
+        setIsDarkMode(prevMode => {
+            const newMode = !prevMode;
+            // Set the theme in localStorage
+            localStorage.setItem('theme', newMode ? 'dark' : 'light');
+            // Update the theme attribute on the <html> element
+            document.documentElement.setAttribute('data-theme', newMode ? 'dark' : 'light');
+            return newMode;
+        });
     };
     
     return (
